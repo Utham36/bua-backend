0a62796a0b0e94713cbefc40ef5ba96898132773
Utham36 <158720462+Utham36@users.noreply.github.com>
2026-01-16 22:01:22 +0100

    Add files via upload

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for core project.
 
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
